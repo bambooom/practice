3ab4ca0dac62c8f5b6fb47748c62fca7e3425f64
bamboo <nmz89825@gmail.com>
2022-12-22 23:44:15 +0800

    add q236.

---
+++ leetcode/236-lowest-common-ancestor.ts
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+// Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+// According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+import { TreeNode } from './util';
+
+function lowestCommonAncestor(
+  root: TreeNode | null,
+  p: TreeNode | null,
+  q: TreeNode | null,
+): TreeNode | null {
+  if (!root) return null;
+  if (p?.val === root.val || q?.val === root.val) {
+    return root;
+  }
+
+  const left = lowestCommonAncestor(root.left, p, q);
+  const right = lowestCommonAncestor(root.right, p, q);
+  return left && right ? root : left || right;
+}
