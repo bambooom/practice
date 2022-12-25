2f1ffaa7844ff9a0008d4b6204c3c0a16412a760
bamboo <nmz89825@gmail.com>
2022-12-25 10:46:34 +0800

    add q347.

---
+++ leetcode/347-top-k-freq-elem.ts
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/top-k-frequent-elements
+// Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+function topKFrequent(nums: number[], k: number): number[] {
+  const map = new Map<number, number>();
+
+  for (let i = 0; i < nums.length; i++) {
+    map.set(nums[i], map.get(nums[i]) ? (map.get(nums[i]) as number) + 1 : 1);
+  }
+
+  const sorted = [...map.entries()].sort((a, b) => b[1] - a[1]);
+
+  const res: number[] = [];
+  for (let i = 0; i < k; i++) {
+    res.push(sorted[i][0]);
+  }
+
+  return res;
+}
