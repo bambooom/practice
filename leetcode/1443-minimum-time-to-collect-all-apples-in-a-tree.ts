1f2dccc763578f0e29228aaf84622d31279a03fc
bamboo <nmz89825@gmail.com>
2023-01-12 00:45:44 +0800

    add q1443, not clear.

---
+++ leetcode/1443-minimum-time-to-collect-all-apples-in-a-tree.ts
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/minimum-time-to-collect-all-apples-in-a-tree/
+
+// Input: n = 7, edges = [[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]], hasApple = [false,false,true,false,true,true,false]
+// Output: 8
+// Explanation: The figure above represents the given tree where red vertices have an apple. One optimal path to collect all apples is shown by the green arrows.
+
+function minTime(n: number, edges: number[][], hasApple: boolean[]): number {
+  const parentMap = new Map<number, number>();
+  let answer = 0;
+  for (let i = 0; i < edges.length; i++) {
+    parentMap.set(edges[i][1], edges[i][0]);
+  }
+  for (let i = 0; i < hasApple.length; i++) {
+    if (hasApple[i]) {
+      let pointer = i;
+      while (pointer !== 0) {
+        const parent = parentMap.get(pointer) as number;
+        answer += 2;
+        if (hasApple[parent]) {
+          break;
+        }
+        hasApple[parent] = true;
+        pointer = parent;
+      }
+    }
+  }
+  return answer;
+}
