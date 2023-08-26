3a961cb63860a7ef82dfdfbfbe4e13eab2abfae0
bamboo <nmz89825@gmail.com>
2023-08-27 00:03:00 +0800

    add q459 better solution.

--- leetcode/459-repeated-substring-pattern.ts
+++ leetcode/459-repeated-substring-pattern.ts
@@ -2,6 +2,8 @@
 // Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
 
 // by math, check the 因数 of the string length
+// Time complexity: O(n⋅sqrt(n))
+// Space complexity: O(n)
 function repeatedSubstringPattern(s: string): boolean {
   const len = s.length;
   const factors: string[] = [];
@@ -19,4 +21,14 @@ function repeatedSubstringPattern(s: string): boolean {
   }
 
   return false;
+}
+
+// better solution is String Concatenation
+// Trick is to duplicate and the remove first and last characters, that would give an indication if string has repeated items
+function repeatedSubstringPattern2(s: string): boolean {
+  return (s + s).slice(1, s.length * 2 - 1).indexOf(s) !== -1;
+}
+
+function repeatedSubstringPattern3(s: string): boolean {
+  return s.repeat(2).slice(1, -1).includes(s);
 }
