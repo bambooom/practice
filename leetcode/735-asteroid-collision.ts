283b56dca13ff777ff6c578246d663369baa08cc
bamboo <nmz89825@gmail.com>
2023-09-16 17:04:43 +0800

    fix q735 solution.

--- leetcode/735-asteroid-collision.ts
+++ leetcode/735-asteroid-collision.ts
@@ -13,35 +13,31 @@ function asteroidCollision(asteroids: number[]): number[] {
   const stack: number[] = [];
 
   for (let i = 0; i < asteroids.length; i++) {
-    const current = asteroids[i];
+    const last = stack[stack.length - 1];
+    const curr = asteroids[i];
 
-    console.log('current: ', current);
+    if (last > 0 && curr < 0) {
+      // colliding
 
-    if (stack.length === 0) {
-      stack.push(current);
-    } else {
-      const last = stack[stack.length - 1];
-      console.log('last: ', last);
-      if (last * current > 0) {
-        // same sign, not colliding
-        stack.push(current);
-      } else {
-        // colliding
-        if (last + current === 0) {
-          stack.pop();
-        } else if (Math.abs(last) < Math.abs(current)) {
-          stack.pop();
-          i--;
-        }
+      if (last + curr === 0) {
+        // last and curr collide and cancel each other out
+        stack.pop();
+      } else if (Math.abs(last) < Math.abs(curr)) {
+        // last and curr collide and last is smaller
+        stack.pop();
+        i--;
       }
+    } else {
+      // not colliding
+      stack.push(curr);
     }
-
-    console.log(stack);
   }
 
   return stack;
 }
 
-console.log(asteroidCollision([-2, -1, 1, 2])); // expected []
+console.log(asteroidCollision([-2, -1, 1, 2])); // expected [-2,-1,1,2]
+// One asteroid in a negative direction and then one in a positive direction will never collide.
+// So in this case, -1 and 1 will never collide. Hence no asteroid is destroyed (other cases being -2,-1 and 1,2 are in the same direction, therefore no collision will occur). If the order was 1 and then -1, then both asteroids would have collided and both would've been destroyed.
 
-// https://leetcode.com/problems/asteroid-collision/solutions/304369/js-javascript-simple-solution-with-single-loop-and-explanation/
+//  But, [1, 2, -2, -1] will collide into []
