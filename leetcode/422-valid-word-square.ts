9fad33f57ad5349ea6cefe0bede42a49a2eddab5
bamboo <nmz89825@gmail.com>
2023-09-26 22:37:08 +0800

    add q422.

---
+++ leetcode/422-valid-word-square.ts
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/valid-word-square/
+// Given an array of strings words, return true if it forms a valid word square.
+// A sequence of strings forms a valid word square if the kth row and column read the same string, where 0 <= k < max(numRows, numColumns).
+
+// direct compare ith row and col string
+function validWordSquare(words: string[]): boolean {
+  for (let i = 0; i < words.length; i++) {
+    const row = words[i];
+    const col = words.map((v) => v[i]).join('');
+    if (row !== col) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function validWordSquare2(words: string[]): boolean {
+  const length = Math.max(...words.map((m) => m.length));
+  let i = 0;
+  while (i < length) {
+    for (let j = 0; j < words.length; j++) {
+      if ((words[i] && !words[j]) || (words[j] && !words[i])) return false;
+      if (words[j][i] !== words[i][j]) return false;
+    }
+    i++;
+  }
+  return true;
+}
