b76f61dd31ab15e92c5a9a7a892936176936a6bd
bamboo <nmz89825@gmail.com>
2023-09-29 21:57:34 +0800

    update another solution for q700.

--- leetcode/700-search-binary-tree.ts
+++ leetcode/700-search-binary-tree.ts
@@ -9,3 +9,17 @@ function searchBST(root: TreeNode | null, val: number): TreeNode | null {
   if (root.val === val) return root;
   return searchBST(root.left, val) || searchBST(root.right, val);
 }
+
+// queue
+function searchBST2(root: TreeNode | null, val: number): TreeNode | null {
+  if (!root) return null;
+  const q = [root];
+  while (q.length) {
+    const node = q.shift() as TreeNode;
+    if (node.val === val) return node;
+    if (node.left) q.push(node.left);
+    if (node.right) q.push(node.right);
+  }
+
+  return null;
+}
