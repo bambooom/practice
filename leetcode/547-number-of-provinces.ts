f36e5b544c11352f7e79b8ad3e7506a10b34f1fc
bamboo <nmz89825@gmail.com>
2023-10-01 19:36:44 +0800

    update q547 comments.

--- leetcode/547-number-of-provinces.ts
+++ leetcode/547-number-of-provinces.ts
@@ -1,5 +1,11 @@
 // https://leetcode.com/problems/number-of-provinces/
-// depth-first search
+// #depth-first search
+
+// There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
+// A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+
+// You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+// Return the total number of provinces.
 
 function findCircleNum(isConnected: number[][]): number {
   const visited: Set<number> = new Set();
