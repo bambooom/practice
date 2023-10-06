06f4e1f73d1aa94600e6d6bf54d02043c670a5da
bamboo <nmz89825@gmail.com>
2023-10-06 11:49:15 +0800

    update one more solution for q215.

--- leetcode/215-kth-largest.ts
+++ leetcode/215-kth-largest.ts
@@ -56,3 +56,23 @@ function findKthLargest(nums: number[], k: number): number {
     [nums[i], nums[j]] = [nums[j], nums[i]];
   }
 }
+
+// https://leetcode.com/problems/kth-largest-element-in-an-array/?envType=study-plan-v2&envId=leetcode-75
+function findKthLargest2(nums: number[], k: number): number {
+  const quickselect = (lo: number, hi: number, K: number): number => {
+    const pivot = hi;
+    let storedIdx = lo;
+    for (let i = lo; i < hi; i++) {
+      if (nums[i] < nums[pivot]) {
+        [nums[i], nums[storedIdx]] = [nums[storedIdx], nums[i]];
+        storedIdx++;
+      }
+    }
+    [nums[pivot], nums[storedIdx]] = [nums[storedIdx], nums[pivot]];
+
+    if (storedIdx === K) return nums[storedIdx];
+    if (K > storedIdx) return quickselect(storedIdx + 1, hi, K);
+    else return quickselect(lo, storedIdx - 1, K);
+  };
+  return quickselect(0, nums.length - 1, nums.length - k);
+}
