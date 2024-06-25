7b5f25f8900f979e506ffb113c4d072b4b758a06
bamboo <nmz89825@gmail.com>
2024-06-25 23:51:54 +0800

    add q1197.

---
+++ leetcode/1197-minimum-knight-moves.ts
@@ -0,0 +1,51 @@
+// https://leetcode.com/problems/minimum-knight-moves
+// In an infinite chess board with coordinates from -infinity to +infinity, you have a knight at square [0, 0].
+// A knight has 8 possible moves it can make, as illustrated below. Each move is two squares in a cardinal direction, then one square in an orthogonal direction.
+// 国际象棋的马的走路规则
+// Return the minimum number of steps needed to move the knight to the square [x, y]. It is guaranteed the answer exists.
+
+// Example 1:
+// Input: x = 2, y = 1
+// Output: 1
+// Explanation: [0, 0] → [2, 1]
+
+// Example 2:
+// Input: x = 5, y = 5
+// Output: 4
+// Explanation: [0, 0] → [2, 1] → [4, 2] → [3, 4] → [5, 5]
+
+// https://leetcode.com/problems/minimum-knight-moves/solutions/4235696/optimized-bfs/?envType=study-plan-v2&envId=premium-algo-100
+function minKnightMoves(x: number, y: number): number {
+  /* eslint-disable */
+  const MOVES = [
+            [2, -1],    [2, 1],
+    [1, -2],                    [1, 2],
+                   //0,0
+    [-1, -2],                   [-1, 2],
+            [-2, -1],   [-2, 1]
+  ];
+  /* eslint-enable */
+  const q: number[][] = [[0, 0]];
+  const seen = new Set<string>();
+
+  let steps = 0;
+  do {
+    const N = q.length;
+    for (let i = 0; i < N; ++i) {
+      const [r, c] = q[i];
+      if (r === y && c === x) {
+        return steps;
+      }
+
+      for (let [dr, dc] of MOVES) {
+        const key = `${(dr += r)},${(dc += c)}`;
+        if (!seen.has(key)) {
+          seen.add(key);
+          q.push([dr, dc]);
+        }
+      }
+    }
+    steps++;
+    q.splice(0, N);
+  } while (q.length > 0);
+}
