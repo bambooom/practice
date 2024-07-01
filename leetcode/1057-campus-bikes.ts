b5a40f5d1ad1aa727d83cc52f513d00c9b1059da
bamboo <nmz89825@gmail.com>
2024-07-01 13:55:18 +0800

    update example for q1167.

--- leetcode/1057-campus-bikes.ts
+++ leetcode/1057-campus-bikes.ts
@@ -8,7 +8,7 @@
 
 // https://leetcode.com/problems/campus-bikes/solutions/413433/clean-javascript-bucket-sort-solution/?envType=study-plan-v2&envId=premium-algo-100
 // bucket sort
-function assignBikes(workers: number[][], bikes: number[][]): number[] {
+function assignBikes1(workers: number[][], bikes: number[][]): number[] {
   const getDist = ([x1, y1]: number[], [x2, y2]: number[]): number =>
     Math.abs(x1 - x2) + Math.abs(y1 - y2);
 
