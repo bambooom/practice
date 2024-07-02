c5d4b199a696e596fe12b261ad29446ec0de1eb9
bamboo <nmz89825@gmail.com>
2024-07-02 11:05:28 +0800

    update link and question desription for q350.

--- leetcode/350-intersection-array-2.ts
+++ leetcode/350-intersection-array-2.ts
@@ -1,3 +1,6 @@
+// https://leetcode.com/problems/intersection-of-two-arrays-ii
+// Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
 /**
  * hashmap methods
  * Time Complexity: O(n+m), where nn and mm are the lengths of the arrays.
