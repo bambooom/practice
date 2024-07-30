0189eaf486fa8c141506a7b32269da138850ff6c
bamboo <nmz89825@gmail.com>
2024-07-30 12:11:15 +0800

    updated 1653.

--- leetcode/1653-minimum-deletions-to-make-string-balanced.ts
+++ leetcode/1653-minimum-deletions-to-make-string-balanced.ts
@@ -1,5 +1,5 @@
 // https://leetcode.com/problems/minimum-deletions-to-make-string-balanced
-// You are given a string s consisting only of characters 'a' and 'b'​​​​
+// You are given a string s consisting only of characters 'a' and 'b'
 // You can delete any number of characters in s to make s balanced. s is balanced if there is no pair of indices (i,j) such that i < j and s[i] = 'b' and s[j]= 'a'.
 // Return the minimum number of deletions needed to make s balanced.
 
@@ -13,6 +13,25 @@
 // Input: s = "bbaaaaabb"
 // Output: 2
 // Explanation: The only solution is to delete the first two characters.
+
+// Approach 1: 3-pass count method
+//  idea is to find the dividing point that minimizes the number of deletions.
+function minimumDeletions3Pass(s: string): number {
+  const n = s.length;
+  const countA = new Array(n + 1).fill(0);
+  const countB = new Array(n + 1).fill(0);
+
+  for (let i = 0; i < n; i++) {
+    countA[i + 1] = countA[i] + (s[i] === 'a' ? 1 : 0);
+    countB[i + 1] = countB[i] + (s[i] === 'b' ? 1 : 0);
+  }
+
+  let minDeletions = Infinity;
+  for (let i = 0; i <= n; i++) {
+    minDeletions = Math.min(minDeletions, countB[i] + (countA[n] - countA[i]));
+  }
+  return minDeletions;
+}
 
 // dynamic programming, stack
 function minimumDeletions(s: string): number {
