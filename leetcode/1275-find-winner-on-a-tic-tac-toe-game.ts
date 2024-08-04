9365940c260b507f270a4c5a4f35a82376c5c7cb
bamboo <nmz89825@gmail.com>
2024-08-04 10:54:30 +0800

    add q1275 description.

--- leetcode/1275-find-winner-on-a-tic-tac-toe-game.ts
+++ leetcode/1275-find-winner-on-a-tic-tac-toe-game.ts
@@ -1,4 +1,13 @@
 // https://leetcode.com/problems/find-winner-on-a-tic-tac-toe-game/
+// Tic-tac-toe is played by two players A and B on a 3 x 3 grid. The rules of Tic-Tac-Toe are:
+// - Players take turns placing characters into empty squares ' '.
+// - The first player A always places 'X' characters, while the second player B always places 'O' characters.
+// - 'X' and 'O' characters are always placed into empty squares, never on filled ones.
+// - The game ends when there are three of the same (non-empty) character filling any row, column, or diagonal.
+// - The game also ends if all squares are non-empty.
+// - No more moves can be played if the game is over.
+// Given a 2D integer array moves where moves[i] = [rowi, coli] indicates that the i-th move will be played on grid[rowi][coli]. return the winner of the game if it exists (A or B). In case the game ends in a draw return "Draw". If there are still movements to play return "Pending".
+// You can assume that moves is valid (i.e., it follows the rules of Tic-Tac-Toe), the grid is initially empty, and A will play first.
 
 // brutal force solution
 function isWin(pos: number[]): boolean {
