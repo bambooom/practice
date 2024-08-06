973b15f3ff81ecaaf2cb4f9bb6c43fbaecaa0662
bamboo <nmz89825@gmail.com>
2024-08-06 10:49:56 +0800

    add q1491 description

--- leetcode/1491-average-salary-excluding-the-minimum-and-maximum-salary.ts
+++ leetcode/1491-average-salary-excluding-the-minimum-and-maximum-salary.ts
@@ -1,4 +1,6 @@
 // https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
+// You are given an array of unique integers salary where salary[i] is the salary of the i-th employee.
+// Return the average salary of employees excluding the minimum and maximum salary. Answers within 10^-5 of the actual answer will be accepted.
 
 function average(salary: number[]): number {
   let min = Number.MAX_VALUE;
