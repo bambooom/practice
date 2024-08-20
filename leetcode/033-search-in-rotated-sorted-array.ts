fdb3d22e66b16bc85a71198746535ae0d509a9b9
bamboo <nmz89825@gmail.com>
2024-08-20 10:35:16 +0800

    add description and comments for q1472 q33

--- leetcode/033-search-in-rotated-sorted-array.ts
+++ leetcode/033-search-in-rotated-sorted-array.ts
@@ -1,5 +1,7 @@
 // https://leetcode.cn/problems/search-in-rotated-sorted-array/
-// Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+// There is an integer array nums sorted in ascending order (with distinct values).
+// Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+// Given the array `nums` after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
 // You must write an algorithm with O(log n) runtime complexity.
 
 // using binary search, When you divide the rotated array into two halves, using mid index,
@@ -22,13 +24,17 @@ function searchRotatedSorterArray(nums: number[], target: number): number {
     if (nums[left] <= nums[mid]) {
       // left side is sorted
       if (nums[left] <= target && target <= nums[mid]) {
+        // then move right pointer so we can work with this part further
         right = mid - 1;
       } else {
         left = mid + 1;
       }
+
+      // if pivot rotation index is in left side, then we work the right side
     } else {
       // right side is sorted
       if (nums[mid] <= target && target <= nums[right]) {
+        // then move left pointer
         left = mid + 1;
       } else {
         right = mid - 1;
--- leetcode/1472-design-browser-history.ts
+++ leetcode/1472-design-browser-history.ts
@@ -1,24 +1,30 @@
 // https://leetcode.com/problems/design-browser-history
+// You have a browser of one tab where you start on the homepage and you can visit another url, get back in the history number of steps or move forward in the history number of steps.
 
 class BrowserHistory {
   history: string[];
   current: number;
+
+  // Initializes the object with the homepage of the browser.
   constructor(homepage: string) {
     this.history = [homepage];
     this.current = 0;
   }
 
+  // Visits url from the current page. It clears up all the forward history.
   visit(url: string): void {
     this.current++;
     this.history = this.history.slice(0, this.current);
     this.history.push(url);
   }
 
+  // Move steps back in history. If you can only return x steps in the history and steps > x, you will return only x steps. Return the current url after moving back in history at most steps.
   back(steps: number): string {
     this.current = Math.max(0, this.current - steps);
     return this.history[this.current];
   }
 
+  // Move steps forward in history. If you can only forward x steps in the history and steps > x, you will forward only x steps. Return the current url after forwarding in history at most steps.
   forward(steps: number): string {
     this.current = Math.min(this.history.length - 1, this.current + steps);
     return this.history[this.current];
@@ -32,3 +38,24 @@ class BrowserHistory {
  * var param_2 = obj.back(steps)
  * var param_3 = obj.forward(steps)
  */
+
+// Example:
+
+// Input:
+// ["BrowserHistory","visit","visit","visit","back","back","forward","visit","forward","back","back"]
+// [["leetcode.com"],["google.com"],["facebook.com"],["youtube.com"],[1],[1],[1],["linkedin.com"],[2],[2],[7]]
+// Output:
+// [null,null,null,null,"facebook.com","google.com","facebook.com",null,"linkedin.com","google.com","leetcode.com"]
+
+// Explanation:
+// BrowserHistory browserHistory = new BrowserHistory("leetcode.com");
+// browserHistory.visit("google.com");       // You are in "leetcode.com". Visit "google.com"
+// browserHistory.visit("facebook.com");     // You are in "google.com". Visit "facebook.com"
+// browserHistory.visit("youtube.com");      // You are in "facebook.com". Visit "youtube.com"
+// browserHistory.back(1);                   // You are in "youtube.com", move back to "facebook.com" return "facebook.com"
+// browserHistory.back(1);                   // You are in "facebook.com", move back to "google.com" return "google.com"
+// browserHistory.forward(1);                // You are in "google.com", move forward to "facebook.com" return "facebook.com"
+// browserHistory.visit("linkedin.com");     // You are in "facebook.com". Visit "linkedin.com"
+// browserHistory.forward(2);                // You are in "linkedin.com", you cannot move forward any steps.
+// browserHistory.back(2);                   // You are in "linkedin.com", move back two steps to "facebook.com" then to "google.com". return "google.com"
+// browserHistory.back(7);                   // You are in "google.com", you can move back only one step to "leetcode.com". return "leetcode.com"
