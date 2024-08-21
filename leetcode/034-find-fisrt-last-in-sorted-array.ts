5f5642da5cfe33fe6c55e6fa1809793b02b4b486
bamboo <nmz89825@gmail.com>
2024-08-21 11:07:31 +0800

    add one more solution to q34

--- leetcode/034-find-fisrt-last-in-sorted-array.ts
+++ leetcode/034-find-fisrt-last-in-sorted-array.ts
@@ -49,3 +49,36 @@ const binary = (nums: number[], target: number): number => {
 
   return -1;
 };
+
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/solutions/1796044/fastest-typescript-binary-search/?envType=study-plan-v2&envId=top-100-liked
+// faster than previous one
+function searchRange2(nums: number[], target: number): number[] {
+  let left = 0;
+  const len = nums.length - 1;
+  let right = len;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      let first = mid - 1;
+      while (nums[first] === target && first >= 0) {
+        first--;
+      }
+
+      let last = mid + 1;
+      while (nums[last] === target && last <= len) {
+        last++;
+      }
+      return [first + 1, last - 1];
+    }
+
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return [-1, -1];
+}
