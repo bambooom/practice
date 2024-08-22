99e44c9aeff75c57295d41faf6c504a46f18451d
bamboo <nmz89825@gmail.com>
2024-08-22 11:34:01 +0800

    Add more solutions for q1056.

--- leetcode/1056-confusing-number.ts
+++ leetcode/1056-confusing-number.ts
@@ -43,3 +43,61 @@ function confusingNumber2(n: number): boolean {
   }
   return rotated !== n;
 }
+
+// https://leetcode.com/problems/confusing-number/solutions/5525030/time-o-log-n-space-o-log-n/?envType=study-plan-v2&envId=programming-skills
+// faster, two pass
+function confusingNumber3(n: number): boolean {
+  const MAP = new Map([
+    ['0', '0'],
+    ['1', '1'],
+    ['6', '9'],
+    ['8', '8'],
+    ['9', '6'],
+  ]);
+
+  const str = n.toString();
+
+  // Check for invalid digits
+  for (let i = 0; i < str.length; ++i) {
+    if (!MAP.has(str[i])) {
+      return false;
+    }
+  }
+
+  // Check number != flipped number
+  let left = 0;
+  for (let right = str.length - 1; left <= right; --right) {
+    if (MAP.get(str[left++]) != str[right]) {
+      return true;
+    }
+  }
+
+  return false;
+}
+// almost same as two pass fast, but a little less memory, single pass
+function confusingNumber4(n: number): boolean {
+  const MAP = new Map([
+    ['0', '0'],
+    ['1', '1'],
+    ['6', '9'],
+    ['8', '8'],
+    ['9', '6'],
+  ]);
+
+  const str = n.toString();
+
+  let left = 0;
+  let isDiff = false;
+  for (let right = str.length - 1; left <= right; --right) {
+    // Check left and right digits are valid
+    const flipped = MAP.get(str[left++]);
+    if (flipped == null || !MAP.has(str[right])) {
+      return false;
+    }
+
+    // Check flipped left digit != right digit
+    isDiff ||= flipped != str[right];
+  }
+
+  return isDiff;
+}
