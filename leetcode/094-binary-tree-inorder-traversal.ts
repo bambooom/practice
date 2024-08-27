59ba7309889932974a7fb8d9dfce23c7b7079fb6
bamboo <nmz89825@gmail.com>
2024-08-27 10:49:17 +0800

    add description for q94.

--- leetcode/094-binary-tree-inorder-traversal.ts
+++ leetcode/094-binary-tree-inorder-traversal.ts
@@ -1,3 +1,6 @@
+// https://leetcode.com/problems/binary-tree-inorder-traversal
+// Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
 import { TreeNode } from './util';
 
 // function inorder(root: TreeNode | null) {
