67211cc4b249f5dbd7aa37d49c7730bb3f5e7513
bamboo <nmz89825@gmail.com>
2024-08-30 12:00:55 +0800

    add one more solution to q102.

--- leetcode/102-binary-tree-level-order-traversal.ts
+++ leetcode/102-binary-tree-level-order-traversal.ts
@@ -1,4 +1,12 @@
 // https://leetcode.com/problems/binary-tree-level-order-traversal/
+// Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[3],[9,20],[15,7]]
+// Input: root = [1]
+// Output: [[1]]
+// Input: root = []
+// Output: []
 
 import { TreeNode } from './util';
 
@@ -27,5 +35,26 @@ function levelOrder(root: TreeNode | null): number[][] {
     levelNodes.push(...nextLevel);
   }
 
+  return result;
+}
+
+// https://leetcode.com/problems/binary-tree-level-order-traversal/solutions/4176806/dfs-super-simple-faster-than-92/?envType=study-plan-v2&envId=top-100-liked
+// dfs solution, recursive, may not be faster, but seems to less memory
+function levelOrder2(root: TreeNode | null): number[][] {
+  const result: number[][] = [];
+
+  const dfs = (root: TreeNode | null, level: number) => {
+    if (!root) return;
+    if (result[level]) {
+      result[level].push(root.val);
+    } else {
+      result[level] = [root.val];
+    }
+
+    dfs(root.left, level + 1);
+    dfs(root.right, level + 1);
+  };
+
+  dfs(root, 0);
   return result;
 }
