29f5da7a1e752f1fa10c3c7ea391d18cad66908f
bamboo <nmz89825@gmail.com>
2024-09-05 13:21:23 +0800

    add one more solution to q5

--- leetcode/005-longest-palindromic-substring.ts
+++ leetcode/005-longest-palindromic-substring.ts
@@ -1,8 +1,9 @@
 // https://leetcode.com/problems/longest-palindromic-substring
 // Given a string s, return the longest palindromic substring in s.
+// A string is palindromic if it reads the same forward and backward.
 // dynamic programming
 
-function longestPalindrome(s: string): string {
+function longestPalindromeQ5(s: string): string {
   if (!s) return '';
   // ll: Left index of the longest palindrome.
   // rr: Right index of the longest palindrome.
@@ -24,3 +25,37 @@ function longestPalindrome(s: string): string {
 }
 
 // https://zkf85.github.io/2019/03/26/leetcode-005-longest-palindrome
+
+// https://leetcode.com/problems/longest-palindromic-substring/solutions/4212570/beats-99-90-optimised-code-o-n-2-time-o-1-space-using-dynamic-programming/?envType=study-plan-v2&envId=top-100-liked
+// faster solution
+function longestPalindromeQ5_2(s: string): string {
+  function expandAroundCenter(left: number, right: number): string {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      left--;
+      right++;
+    }
+    return s.substring(left + 1, right);
+  }
+
+  let longest = '';
+
+  for (let i = 0; i < s.length; i++) {
+    const odd = expandAroundCenter(i, i);
+    const even = expandAroundCenter(i, i + 1);
+
+    if (odd.length > longest.length) {
+      longest = odd;
+    }
+
+    if (even.length > longest.length) {
+      longest = even;
+    }
+  }
+
+  return longest;
+}
+
+// // Example usage
+// const s = 'babad';
+// const result = longestPalindrome(s);
+// console.log('Longest Palindromic Substring: ' + result);
