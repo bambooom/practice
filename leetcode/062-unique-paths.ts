3b5b2ea5c53d6071df186e0c2287e0ad80f556dc
bamboo <nmz89825@gmail.com>
2024-09-06 11:24:35 +0800

    add description for q62

--- leetcode/062-unique-paths.ts
+++ leetcode/062-unique-paths.ts
@@ -1,4 +1,7 @@
 // https://leetcode.com/problems/unique-paths/
+// There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+// Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+// The test cases are generated so that the answer will be less than or equal to 2 * 10^9
 // #dynamic-programming #combination
 
 // DP space optimized
@@ -6,7 +9,7 @@ function uniquePaths(m: number, n: number): number {
   const dp = new Array(n + 1).fill(1);
   for (let row = m - 1; row > 0; row--) {
     for (let col = n - 1; col > 0; col--) {
-      dp[col] = dp[col] + dp[col + 1];
+      dp[col] += dp[col + 1];
     }
   }
   return dp[1];
