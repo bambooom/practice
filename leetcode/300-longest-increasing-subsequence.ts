0842874d731faf159ae28debce1eb9cf9135138b
bamboo <nmz89825@gmail.com>
2024-09-09 11:18:42 +0800

    update q300

--- leetcode/300-longest-increasing-subsequence.ts
+++ leetcode/300-longest-increasing-subsequence.ts
@@ -2,6 +2,17 @@
 // Given an integer array nums, return the length of the longest strictly increasing subsequence.
 // #binary-search, #dynamic-programming
 
+// Example 1:
+// Input: nums = [10,9,2,5,3,7,101,18]
+// Output: 4
+// Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+// Example 2:
+// Input: nums = [0,1,0,3,2,3]
+// Output: 4
+// Example 3:
+// Input: nums = [7,7,7,7,7,7,7]
+// Output: 1
+
 // DP way. https://leetcode.com/problems/longest-increasing-subsequence/solutions/1680012/javascript-dp-solution-with-written-intuition/
 /**
  *
@@ -24,9 +35,9 @@ function lengthOfLIS(nums: number[]): number {
     for (let j = 0; j < i; j++) {
       if (nums[j] < nums[i]) {
         dp[i] = Math.max(dp[i], dp[j] + 1);
-        longest = Math.max(longest, dp[i]);
       }
     }
+    longest = Math.max(longest, dp[i]);
   }
   return longest;
 }
