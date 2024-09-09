f5cf3f916143a361c1ddba4931ff77bd3f8bf2fe
bamboo <nmz89825@gmail.com>
2024-09-09 11:14:43 +0800

    add more solutions to q198

--- leetcode/198-house-robber.ts
+++ leetcode/198-house-robber.ts
@@ -33,3 +33,48 @@ function rob(nums: number[]): number {
 
   return robFrom(0);
 }
+
+// https://leetcode.com/problems/house-robber/solutions/4605837/time-o-n-space-o-1/?envType=study-plan-v2&envId=top-100-liked
+function rob_2(nums: number[]): number {
+  let robber1 = 0;
+  let robber2 = 0;
+  for (const num of nums) {
+    const temp = Math.max(robber1 + num, robber2);
+    robber1 = robber2;
+    robber2 = temp;
+  }
+
+  return robber2;
+}
+// https://leetcode.com/problems/house-robber/solutions/4065058/dp-both-approaches-o-n/?envType=study-plan-v2&envId=top-100-liked
+// tabulation
+function rob_3(nums: number[]): number {
+  const n = nums.length;
+  const state = new Array(n).fill(-1);
+  //if there is only one house im just robbing that
+  state[n - 1] = nums[n - 1];
+  //if there are only 2 houses i would rob the one which gives me max
+  state[n - 2] = Math.max(nums[n - 1], nums[n - 2]);
+  for (let i = n - 3; i >= 0; i--) {
+    //else i would be having 2 chocies either to rob a house or skip it
+    state[i] = Math.max(nums[i] + state[i + 2], state[i + 1]);
+  }
+  return state[0];
+}
+// rec memo
+function rob_4(nums: number[]): number {
+  const dp = new Array(nums.length).fill(-1);
+  const robber = (nums: number[], i: number): number => {
+    if (i >= nums.length) {
+      return 0;
+    }
+    if (dp[i] !== -1) {
+      return dp[i];
+    }
+    return (dp[i] = Math.max(
+      nums[i] + robber(nums, i + 2),
+      robber(nums, i + 1),
+    ));
+  };
+  return robber(nums, 0);
+}
