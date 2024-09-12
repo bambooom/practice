00177b80ba2f90e6414db83bd920f587adaac669
bamboo <nmz89825@gmail.com>
2024-09-12 10:53:50 +0800

    add one more solution to q1143

--- leetcode/1143-longest-common-subsequence.ts
+++ leetcode/1143-longest-common-subsequence.ts
@@ -63,3 +63,23 @@ function longestCommonSubsequence2(text1: string, text2: string): number {
 
   return dp[h - 1][w - 1];
 }
+
+// https://leetcode.com/problems/longest-common-subsequence/solutions/4531610/dp-bottom-up/?envType=study-plan-v2&envId=top-100-liked
+function longestCommonSubsequence3(text1: string, text2: string): number {
+  let dp = Array(text1.length + 1).fill(0);
+
+  for (let j = text2.length - 1; j >= 0; j--) {
+    const curr = Array(text1.length + 1).fill(0);
+    for (let i = text1.length - 1; i >= 0; i--) {
+      if (text1[i] === text2[j]) {
+        curr[i] = dp[i + 1] + 1;
+      } else {
+        curr[i] = Math.max(curr[i + 1], dp[i]);
+      }
+    }
+
+    dp = curr;
+  }
+
+  return dp[0];
+}
