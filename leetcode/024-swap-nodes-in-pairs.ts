19ffafe9121e5a55ef4eb5e2640255e5b25f2f1c
bamboo <nmz89825@gmail.com>
2024-10-14 11:23:12 +0800

    update q24

--- leetcode/024-swap-nodes-in-pairs.ts
+++ leetcode/024-swap-nodes-in-pairs.ts
@@ -1,4 +1,5 @@
 // https://leetcode.com/problems/swap-nodes-in-pairs/
+// Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
 
 import { ListNode } from './util';
 
