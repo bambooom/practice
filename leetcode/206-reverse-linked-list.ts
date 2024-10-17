2f98cf77016195ab1b8a1e33d3c3c1dffae821c1
bamboo <nmz89825@gmail.com>
2024-10-17 10:36:49 +0800

    update q206 q234

--- leetcode/206-reverse-linked-list.ts
+++ leetcode/206-reverse-linked-list.ts
@@ -1,4 +1,13 @@
 // https://leetcode.com/problems/reverse-linked-list/
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+// Example1:
+// Input: head = [1,2,3,4,5]
+// Output: [5, 4, 3, 2, 1]
+
+// Example2:
+// Input: head = [1,2]
+// Output: [2,1]
 
 import { ListNode } from './util';
 
--- leetcode/234-palindrome-linked-list.ts
+++ leetcode/234-palindrome-linked-list.ts
@@ -1,3 +1,14 @@
+// https://leetcode.com/problems/palindrome-linked-list/
+// Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+// Example 1:
+// Input: head = [1,2,2,1]
+// Output: true
+
+// Example 2:
+// Input: head = [1,2]
+// Output: false
+
 import { ListNode } from './util';
 
 // Copy into Array List and then Use Two Pointer Technique
