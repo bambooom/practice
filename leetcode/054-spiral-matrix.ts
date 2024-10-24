df8f9e4f3e42ccb2da7b47c3d958b3e81194bc7b
bamboo <nmz89825@gmail.com>
2024-10-24 10:33:53 +0800

    update q54

--- leetcode/054-spiral-matrix.ts
+++ leetcode/054-spiral-matrix.ts
@@ -9,6 +9,22 @@
 
 // Time Complexity: O(m*n)
 // Space Complexity: O(1)
+
+// Example1:
+// Input: matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+// Output: [1, 2, 3, 6, 9, 8, 7, 4, 5];
+
+// Example2: Input: matrix = [
+//   [1, 2, 3, 4],
+//   [5, 6, 7, 8],
+//   [9, 10, 11, 12],
+// ];
+// Output: [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7];
+
 function spiralOrder(matrix: number[][]): number[] {
   if (matrix.length === 0) return [];
   const res: number[] = [];
