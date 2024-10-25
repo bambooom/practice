6d31187ade4c99710a8e48cc4233ce51cc8d90de
bamboo <nmz89825@gmail.com>
2024-10-25 11:08:58 +0800

    update q240

--- leetcode/240-search-2d-matrix.ts
+++ leetcode/240-search-2d-matrix.ts
@@ -1,4 +1,7 @@
 // https://leetcode.com/problems/search-a-2d-matrix-ii/
+// Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+// Integers in each row are sorted in ascending from left to right.
+// Integers in each column are sorted in ascending from top to bottom.
 
 // binary search
 function searchMatrix(matrix: number[][], target: number): boolean {
