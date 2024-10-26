d4d4699331f818717413459516933af535ac92d6
bamboo <nmz89825@gmail.com>
2024-10-26 12:14:23 +0800

    update description for q20 q3

--- leetcode/003-longest-substr-uniq-char.ts
+++ leetcode/003-longest-substr-uniq-char.ts
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
 // Given a string s, find the length of the longest substring without repeating characters.
 
 // brute force, Time O(n^3), space O(min(n,m))
