fa232516c81c908bf42c5dd4651ee7aef0bba982
bamboo <nmz89825@gmail.com>
2025-01-09 22:23:26 +0800

    add description for q283.

--- leetcode/283-move-zeros.ts
+++ leetcode/283-move-zeros.ts
@@ -1,3 +1,7 @@
+// https://leetcode.com/problems/move-zeroes/description/
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+// Note that you must do this in-place without making a copy of the array.
+
 // #twp pointers
 // Input: nums = [0,1,0,3,12]
 // Output: [1,3,12,0,0]
