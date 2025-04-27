a198cc63976cbe80deb627784b098249ef8ec8cf
bamboo <nmz89825@gmail.com>
2025-04-27 10:23:50 +0800

    add q3392

---
+++ leetcode/3392-count-subarrays-of-length-three-with-a-condition.ts
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/count-subarrays-of-length-three-with-a-condition
+// Given an integer array nums, return the number of subarrays of length 3 such that the sum of the first and third numbers equals exactly half of the second number.
+
+// Example 1:
+// Input: nums = [1,2,1,4,1]
+// Output: 1
+// Explanation:
+// Only the subarray [1,4,1] contains exactly 3 elements where the sum of the first and third numbers equals half the middle number.
+
+// Example 2:
+// Input: nums = [1,1,1]
+// Output: 0
+// Explanation:
+// [1,1,1] is the only subarray of length 3. However, its first and third numbers do not add to half the middle number.
+
+function countSubarrays(nums: number[]): number {
+  let count = 0;
+  for (let i = 1; i < nums.length - 1; i++) {
+    if (nums[i - 1] + nums[i + 1] === nums[i] / 2) {
+      count++;
+    }
+  }
+  return count;
+}
