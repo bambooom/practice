eda157d7f8705149976d465e21d2a04c80e3db32
bamboo <nmz89825@gmail.com>
2025-08-15 10:39:44 +0800

    add more solutions to q342

--- leetcode/342-power-of-four.ts
+++ leetcode/342-power-of-four.ts
@@ -18,3 +18,16 @@ function isPowerOfFour(n: number): boolean {
 function isPowerOfFour2(n: number): boolean {
   return Number.isInteger(Math.log(n) / Math.log(4));
 }
+
+// bit mask, checking if n = 4^x = 2^2x, so n must be a power of 2.
+// use bit mastk 0x55555555 to check if n is power of 4
+function isPowerOfFour3(n: number): boolean {
+  return (n & 0x55555555) !== 0 && (n & (n - 1)) === 0;
+}
+
+// bit mask
+// Use the invert of the bit mask to check if n is a power of 4.
+// ~0x55555555 is 0xaaaaaaaa
+function isPowerOfFour4(n: number): boolean {
+  return n > 0 && (n & 0xaaaaaaaa) === 0 && (n & (n - 1)) === 0;
+}
