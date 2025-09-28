b84e2d4956b15c03d946fe41757ff5add6e59973
bamboo <nmz89825@gmail.com>
2025-09-28 12:01:46 +0800

    update example for q976.

--- leetcode/976-largest-perimeter-triangle.ts
+++ leetcode/976-largest-perimeter-triangle.ts
@@ -3,6 +3,21 @@
 // Given an integer array nums, return the largest perimeter（周长） of a triangle with a non-zero area, formed from three of these lengths. If it is impossible to form any triangle of a non-zero area, return 0.
 // triangle: a + b > c
 
+// Example 1:
+
+// Input: nums = [2,1,2]
+// Output: 5
+// Explanation: You can form a triangle with three side lengths: 1, 2, and 2.
+// Example 2:
+
+// Input: nums = [1,2,1,10]
+// Output: 0
+// Explanation:
+// You cannot use the side lengths 1, 1, and 2 to form a triangle.
+// You cannot use the side lengths 1, 1, and 10 to form a triangle.
+// You cannot use the side lengths 1, 2, and 10 to form a triangle.
+// As we cannot use any three side lengths to form a triangle of non-zero area, we return 0.
+
 function largestPerimeter(nums: number[]): number {
   nums.sort((a, b) => b - a); // desc
   // check from biggest number, if we have a + b > c, return the sum
