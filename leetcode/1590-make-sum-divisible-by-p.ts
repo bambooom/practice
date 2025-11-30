20edc496904aa9a2eb442dfff995519677410c25
bamboo <nmz89825@gmail.com>
2025-11-30 21:45:21 +0800

    Add one more solution to q1590

--- leetcode/1590-make-sum-divisible-by-p.ts
+++ leetcode/1590-make-sum-divisible-by-p.ts
@@ -54,3 +54,24 @@ function minSubarray(nums: number[], p: number): number {
   // Return the minimum length of the subarray, or -1 if no subarray needs to be removed
   return minLen === Infinity || minLen === nums.length ? -1 : minLen;
 }
+
+function minSubarray2(nums: number[], p: number): number {
+  let total = nums.reduce((a, b) => a + b, 0);
+  let need = total % p;
+  if (need === 0) return 0;
+
+  let mp = new Map<number, number>();
+  mp.set(0, -1);
+
+  let prefix = 0;
+  let res = nums.length;
+
+  for (let i = 0; i < nums.length; i++) {
+    prefix = (prefix + nums[i]) % p;
+    let target = (prefix - need + p) % p;
+    if (mp.has(target)) res = Math.min(res, i - mp.get(target)!);
+    mp.set(prefix, i);
+  }
+
+  return res === nums.length ? -1 : res;
+}
