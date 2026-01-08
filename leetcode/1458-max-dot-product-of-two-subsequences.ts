0366bf08f128abb9fcab23f27d796fd21b942ca1
bamboo <nmz89825@gmail.com>
2026-01-08 10:49:19 +0800

    update comments for q1458

--- leetcode/1458-max-dot-product-of-two-subsequences.ts
+++ leetcode/1458-max-dot-product-of-two-subsequences.ts
@@ -4,11 +4,27 @@
 // Return the maximum dot product between non-empty subsequences of nums1 and nums2 with the same length.
 // A subsequence of a array is a new array which is formed from the original array by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, [2,3,5] is a subsequence of [1,2,3,4,5] while [1,5,3] is not).
 
-// Example
+// Example 1:
 // Input: nums1 = [2,1,-2,5], nums2 = [3,0,-6]
 // Output: 18
 // Explanation: Take subsequence [2,-2] from nums1 and subsequence [3,-6] from nums2.
 // Their dot product is (2*3 + (-2)*(-6)) = 18.
+
+// Example 2:
+// Input: nums1 = [3,-2], nums2 = [2,-6,7]
+// Output: 21
+// Explanation: Take subsequence [3] from nums1 and subsequence [7] from nums2.
+// Their dot product is (3*7) = 21.
+
+// Example 3:
+// Input: nums1 = [-1,-1], nums2 = [1,1]
+// Output: -1
+// Explanation: Take subsequence [-1] from nums1 and subsequence [1] from nums2.
+// Their dot product is -1.
+
+// Constraints:
+// 1 <= nums1.length, nums2.length <= 500
+// -1000 <= nums1[i], nums2[i] <= 1000
 
 // https://leetcode.com/problems/max-dot-product-of-two-subsequences/?envType=daily-question&envId=2023-10-08
 // recursive dynamic programming approach with memoization
@@ -19,10 +35,14 @@
 function maxDotProduct(nums1: number[], nums2: number[]): number {
   const n = nums1.length,
     m = nums2.length;
+
+  // Initialize a 2D array to store the maximum dot product for each subarray
   const dp = new Array(n + 1)
     .fill(-Infinity)
     .map(() => Array(m + 1).fill(-Infinity));
 
+  // helper function recursively calculates the maximum dot product for a given subarray
+  // use memoization to avoid redundant calculations
   const iterate = (i: number, j: number): number => {
     if (i >= n || j >= m) {
       return -Infinity; // Base case: if we go beyond the array bounds, return negative infinity.
