65e99b6c7921dcba00e8fc70c93488f2f0823557
bamboo <nmz89825@gmail.com>
2026-03-11 10:21:52 +0800

    add q1009

---
+++ leetcode/1009-complement-of-base-10-integer.ts
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/complement-of-base-10-integer/description
+// The complement of an integer is the integer you get when you flip all the 0's to 1's and all the 1's to 0's in its binary representation.
+// For example, The integer 5 is "101" in binary and its complement is "010" which is the integer 2.
+// Given an integer n, return its complement.
+
+// Example 1:
+// Input: n = 5
+// Output: 2
+// Explanation: 5 is "101" in binary, with complement "010" in binary, which is 2 in base-10.
+
+// Example 2:
+// Input: n = 7
+// Output: 0
+// Explanation: 7 is "111" in binary, with complement "000" in binary, which is 0 in base-10.
+
+// Example 3:
+// Input: n = 10
+// Output: 5
+// Explanation: 10 is "1010" in binary, with complement "0101" in binary, which is 5 in base-10.
+
+// Constraints:
+// 0 <= n < 10^9
+
+function bitwiseComplement(n: number): number {
+  const binary = n.toString(2);
+  const sum = parseInt('1'.repeat(binary.length), 2); // find the sum in binary and convert in to decimal
+  return sum - n;
+}
+
+// more straightforward
+function bitwiseComplement2(n: number): number {
+  const s = n.toString(2);
+  let x = '';
+  for (const v of s) {
+    x += v === '1' ? '0' : '1';
+  }
+  return parseInt(x, 2);
+}
